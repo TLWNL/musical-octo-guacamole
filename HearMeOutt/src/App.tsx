ee5fdaa4ce62cf7e8768e77b1c5cadbc328b492b
TLWNL <dylanrijnsburger@gmail.com>
2025-02-01 10:04:42 -0500

    Lint again

--- HearMeOutt/src/App.tsx
+++ HearMeOutt/src/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import AccountRegistration from "./Components/AccountRegistration";
 import LoginPage from "./Components/LoginPage";
-import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RegistrationBus from "./Components/RegistrationBus";
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/gebruiker/aanmaken" element={<AccountRegistration />} />
+        <Route path="/bedrijf/aanmaken" element={<RegistrationBus />} />
       </Routes>
     </Router>
   );
---
+++ HearMeOutt/src/Components/RegistrationBus.tsx
@@ -0,0 +1,5 @@
+function RegistrationBus() {
+  return <></>;
+}
+
+export default RegistrationBus;
